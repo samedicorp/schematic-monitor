e2287acabbfc48d3e0fca48d1fa9f2d67b25027a
Sam Deane <sam@elegantchaos.com>
2022-10-09 14:11:05 +0100

    iterate industry

--- main.lua
+++ main.lua
@@ -17,6 +17,7 @@ function Module:onStart()
     debugf("Schematic Monitor started.")
 
     self:attachToScreen()
+    self:scanIndustry()
 end
 
 function Module:onStop()
@@ -43,22 +44,53 @@ function Module:attachToScreen()
     end
 end
 
+function Module:scanIndustry()
+    local core = modula.core
+    local elementIDs = core.getElementIdList()
+    for elementID in ipairs(elementIDs) do
+        local info = core.getElementIndustryInfoById(elementID)
+        if info and info.state and info.state > 0 then
+            local name = core.getElementDisplayNameById(elementID)
+
+            local product = info.currentProducts[1]
+            if product then
+                local productItem = system.getItem(product.id)
+                local name = productItem.locDisplayName
+                local icon = productItem.iconPath
+                local schematic = productItem.schematics[1]
+                local state = info.state
+                if state == 2 then
+                    local remaining = info.schematicsRemaining
+                    if (remaining > 0) and (remaining < 10) then
+                        debugf("%s: Schematics Low (%s)", name, remaining)
+                    end
+                elseif state == 7 then
+                    debugf("%s: Out Of Schematics", name)
+                    debugf(core.getElementClassById(elementID))
+                    debugf(productItem)
+                end
+            end
+
+        end
+    end
+
+end
+
 Module.renderScript = [[
 
-containers = containers or {}
+state = state or { lines = { "hello world" }}
 
 if payload then
-    local name = payload.name
-    if name then
-        containers[name] = payload
+    local text = payload.text
+    if text then
+        table.append(state.lines, text)
     end
-    reply = { name = name, result = "ok" }
+    reply = { result = "ok" }
 end
 
 local screen = toolkit.Screen.new()
 local layer = screen:addLayer()
 
-state = state or { lines = { "hello world" }}
 local chart = layer:addField(layer.rect:inset(10), state)
 
 layer:render()
